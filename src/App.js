c31a2e620eb5fdca1b6132eeeb31dcb3557eb931
Jorge Lameiro <jlameiro@farelogix.com>
2021-06-11 16:43:00 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './actions';
+
+function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="App">
+      <h1>Counter {counter}</h1>
+      <button onClick={() => dispatch(increment(1))}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      {
+        isLogged ? <h3>Valuable information for logged users</h3> : ''
+      }
+    </div>
+  )
+}
+
+export default App;
